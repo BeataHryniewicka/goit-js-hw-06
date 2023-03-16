e0ee1f4865af30c034cb1bcf1e674a9390766075
BeataHryniewicka <118068394+BeataHryniewicka@users.noreply.github.com>
2023-03-16 19:55:46 +0100

    .

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,14 +1,20 @@
-const counterValue = 0;
-const spanValue = document.querySelcetor(`#value`);
-const addCounterBtn = document.querySelector('[data-action="increment"]');
-const removeCounterBtn = document.querySelector('[data-action="decrement"]');
+let counterValue = 0;
+let spanValue = document.querySelector("#value");
 
-addCounterBtn.addEventListener("click", () => {
+let addCounterBtn = document.querySelector('[data-action="increment"]');
+let removeCounterBtn = document.querySelector('[data-action="decrement"]');
+
+function incrementFn() {
   counterValue = counterValue + 1;
   spanValue.textContent = counterValue;
-});
-
-removeCounterBtn.addEventListener("click", () => {
+  console.log(`counterValuePlus`);
+}
+function decrementFn() {
   counterValue = counterValue - 1;
   spanValue.textContent = counterValue;
-});
+  console.log(`counterValueMinus`);
+}
+
+addCounterBtn.addEventListener("click", incrementFn);
+
+removeCounterBtn.addEventListener("click", decrementFn);
