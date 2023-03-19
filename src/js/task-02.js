53dc0af5067822132e9dc8ca16c6a73c480197c2
BeataHryniewicka <118068394+BeataHryniewicka@users.noreply.github.com>
2023-03-19 23:35:35 +0100

    task 10

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,3 +1,10 @@
+// Napisz skrypt, który dla każdego elementu tablicy ingredients:
+// Utworzy oddzielny element <li>. Koniecznie użyj metody document.createElement().
+// Doda nazwę elementu jako jego zawartość tekstową.
+// Doda do elementu klasę item.
+// Po czym umieści wszystkie <li> na liście podczas jednej operacji ul#ingredients.
+
+
 const ingredients = [
   "Potatoes",
   "Mushrooms",
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,5 +1,41 @@
+//Napisz skrypt tworzenia i usuwania kolekcji elementów. Użytkownik wprowadza liczbę elementów do input i naciska przycisk Utwórz, co rozpoczyna tworzenie się kolekcji. Po naciśnięciu przycisku Usuń, kolekcja elementów zostaje usunięta.
+//Wymiary pierwszego <div> - 30px na 30px.
+// Każdy następny element powinien być szerszy i wyższy od poprzedniego o 10px.
+// Wszystkie elementy powinny mieć losowy kolor tła w formacie NEX. Użyj gotowej funkcji getRandomHexColor aby otrzymać kolor.
+// Utwórz funkcję destroyBoxes(), która usuwa zawartość div#boxes, tym samym usuwając wszystkie utworzone elementy.
+
+// div.innerHTML = `width =30px height =30px`
+// const element = document. createElement("div");
+// div.append(element);
+
+//.................................
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+const divNew = document.querySelector("#boxes");
+let divWidth = (divNew.style.width = `30px`);
+let divHeight = (divNew.style.height = `30px`);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+btnCreate.addEventListener(`click`, () => {
+  divNew.style.backgroundColor = getRandomHexColor();
+  const el = document.createElement("div");
+  let elvWidth = (el.style.width = `30px`);
+  let elHeight = (el.style.height = `30px`);
+  el.classList.add("element");
+  el.style.backgroundColor = getRandomHexColor();
+  divNew.appendChild(el);
+});
+
+// btnDestroy.addEventListener(`click`, () => {
+//   const el = document.createElement("div");
+//   let elvWidth = (el.style.width = `30px`);
+//   let elHeight = (el.style.height = `30px`);
+//   el.style.backgroundColor = getRandomHexColor();
+
+//   divNew.appendChild(el);
+// });
